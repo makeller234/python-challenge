d0ca16eb7224fbd78197f888471270d5ebe5a677
Melissa Keller <kelle595@umn.edu>
2019-12-17 17:05:45 -0600

    updates to PyBank main

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,30 +10,44 @@ with open(csvpath, newline='') as budgetFile:
 
     totalMonths = 0
     totalMoney = 0.0
+    
 
-    data= []
-
-
+    data= {}
+    values = []
+    monies = 0
     for row in csvreader:
-        data.append(row)
-        totalMonths += 1
         totalMoney += float(row[1])
+        totalMonths += 1
+        values.append(row[1])
+        #data = {row[0]:row[1] for row in csvreader}
+        
 
-        for x in data:
-            greatProfits = x[2]
-            
+    
+    for item in values[::-1]:
+        a = len(values)-1
+
+        monies = float(item)
+        monies = monies -float(values[a])
+        a-=1
+        averageProfLoss = monies/(totalMonths-1)
+    print(averageProfLoss)
+
+
+    
+
+        
 
 '''
 for x in data:
     totalMonths +=1
     totalMoney += float(x[1])
-
     greatProfits = float(x[1])
     if float(x[1])>:
         greatProfits = float(x[2][1])-float(x[1])
     else:
         greatProfits = greatProfits
 '''
+
 
 
 
