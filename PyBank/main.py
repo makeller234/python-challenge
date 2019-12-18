131a6dd60e8514d175c38c30c7de814436dab062
Melissa Keller <kelle595@umn.edu>
2019-12-18 09:34:00 -0600

    updates to PyBank main

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,7 +12,7 @@ with open(csvpath, newline='') as budgetFile:
     totalMoney = 0.0
     
 
-    data= {}
+    #data= {}
     values = []
     monies = 0
     for row in csvreader:
@@ -20,17 +20,35 @@ with open(csvpath, newline='') as budgetFile:
         totalMonths += 1
         values.append(row[1])
         #data = {row[0]:row[1] for row in csvreader}
-        
 
     
     for item in values[::-1]:
         a = len(values)-1
+        
+        for item1 in values:
+            monies = float(item)
+            monies = monies -float(values[a])
+            a-=1
+            averageProfLoss = "{:.2f}".format(monies/(totalMonths-1))
+        break
 
-        monies = float(item)
-        monies = monies -float(values[a])
-        a-=1
-        averageProfLoss = monies/(totalMonths-1)
-    print(averageProfLoss)
+    money = float(item)  #money doens't change, so this needs to be moved and/or updated somehow
+    dec = 0 #money - float(values[a-1])
+    a = 0
+    for item2 in values:
+
+        print(f'{a} loops money= {money}')
+        if money-float(values[a-1]) < dec:
+            dec = money-float(values[a-1])
+        else:
+            dec = dec
+        a+=1
+
+
+ 
+    #print(averageProfLoss)
+    #print("{:.0f}".format(totalMoney))
+    #print(totalMonths)
 
 
     
